0f56dcf0785e70d6cef2963129d18e931ce565df
dolveda <dolveda0@gmail.com>
2023-06-01 12:07:39 -0700

    Add finch index feature, view, and template

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
+finches = [
+    {'name': 'Tweety', 'breed': 'House Finch', 'description': 'Loves to trick the cat', 'age': 1},
+    {'name': 'Polly', 'breed': 'Purple Finch', 'description': 'Talkative and sarcastic', 'age': 2},
+]
+
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {'finches': finches})
