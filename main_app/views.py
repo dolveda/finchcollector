e3cb1e5f0322af03be24abf2f660eaf458518ea4
dolveda <dolveda0@gmail.com>
2023-06-04 12:16:26 -0700

    complete part 2

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
-
-finches = [
-    {'name': 'Tweety', 'breed': 'House Finch', 'description': 'Loves to trick the cat', 'age': 1},
-    {'name': 'Polly', 'breed': 'Purple Finch', 'description': 'Talkative and sarcastic', 'age': 2},
-]
+from.models import Finch
 
 def home(request):
     return render(request, 'home.html')
@@ -11,6 +7,10 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', {'finches': finches})
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', { 'finch': finch })
